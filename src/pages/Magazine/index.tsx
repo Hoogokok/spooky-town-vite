9cddeb6578908278bfbf6e723a3adf18820a3f42
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2025-01-08 08:55:10 +0900

    feat(magazine): 매거진 페이지 기본 구조 구현
    - Article 타입 정의
    - Magazine 컴포넌트 기본 구조 생성
    - 기본 레이아웃 스타일 적용

---
+++ src/pages/Magazine/index.tsx
@@ -0,0 +1,15 @@
+import './magazine.css'
+import { Article } from '../../types/article'
+
+function Magazine() {
+    return (
+        <div className="magazineContainer">
+            <h1 className="magazineTitle">Fangoria</h1>
+            <div className="articlesGrid">
+                {/* 아티클 목록은 추후 구현 */}
+            </div>
+        </div>
+    )
+}
+
+export default Magazine 
