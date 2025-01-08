88aa377fe849c6e002f089b2227dcbfa4e67b944
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2025-01-08 09:27:33 +0900

    feat(router): 매거진 페이지 라우트 추가
    - App 컴포넌트에 매거진 페이지 라우트 설정
    - /magazine 경로에 Magazine 컴포넌트 연결

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import MainLayout from './components/layout/MainLayout'
 import Signup from './pages/Signup'
+import Magazine from './pages/Magazine'
 
 const queryClient = new QueryClient()
 
@@ -17,6 +18,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
+            <Route path="/magazine" element={<Magazine />} />
           </Routes>
         </MainLayout>
       </BrowserRouter>
