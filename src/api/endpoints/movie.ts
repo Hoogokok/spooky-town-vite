d63992ab77e9c517a3e47d2e09bb522d81b70ecf
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2025-01-09 13:28:55 +0900

    feat(API): 영화 상세정보 조회 API 추가
    - 스트리밍 영화 상세정보 조회 API 추가
    - 극장 영화 상세정보 조회 API 추가
    - parseResponse 함수를 제네릭으로 변경하여 재사용성 개선

--- src/api/endpoints/movie.ts
+++ src/api/endpoints/movie.ts
@@ -1,5 +1,5 @@
 import { Effect } from 'effect'
-import { TheaterMovie } from '../../types/api/movie'
+import { TheaterMovie, MovieDetail, StreamingMovieDetail, TheaterMovieDetail } from '../../types/api/movie'
 
 export class FetchError {
     readonly _tag = 'FetchError'
@@ -23,8 +23,8 @@ const fetchFromApi = (endpoint: string) => Effect.tryPromise({
     catch: () => new NetworkError('네트워크 오류가 발생했습니다')
 })
 
-const parseResponse = (response: Response) => Effect.tryPromise({
-    try: () => response.json() as Promise<TheaterMovie[]>,
+const parseResponse = <T>(response: Response) => Effect.tryPromise({
+    try: () => response.json() as Promise<T>,
     catch: () => new FetchError('응답을 파싱하는데 실패했습니다')
 })
 
@@ -38,11 +38,43 @@ const fetchMovies = (endpoint: string): Effect.Effect<TheaterMovie[], FetchError
             ))
         }
 
-        const movies = yield* _(parseResponse(response))
+        const movies = yield* _(parseResponse<TheaterMovie[]>(response))
         return movies
+    })
+
+// 스트리밍 영화 상세 정보 조회
+const fetchStreamingMovieDetail = (id: string): Effect.Effect<StreamingMovieDetail, FetchError | NetworkError, never> =>
+    Effect.gen(function* (_) {
+        const response = yield* _(fetchFromApi(`/movies/streaming/${id}`))
+
+        if (!response.ok) {
+            return yield* _(Effect.fail<FetchError | NetworkError>(
+                new FetchError('영화 상세 정보를 가져오는데 실패했습니다')
+            ))
+        }
+
+        const movie = yield* _(parseResponse<StreamingMovieDetail>(response))
+        return movie
+    })
+
+// 극장 영화 상세 정보 조회
+const fetchTheaterMovieDetail = (id: string): Effect.Effect<TheaterMovieDetail, FetchError | NetworkError, never> =>
+    Effect.gen(function* (_) {
+        const response = yield* _(fetchFromApi(`/movies/theater/${id}`))
+
+        if (!response.ok) {
+            return yield* _(Effect.fail<FetchError | NetworkError>(
+                new FetchError('영화 상세 정보를 가져오는데 실패했습니다')
+            ))
+        }
+
+        const movie = yield* _(parseResponse<TheaterMovieDetail>(response))
+        return movie
     })
 
 export const movieApi = {
     fetchUpcomingMovies: () => fetchMovies('/movies/theater/upcoming'),
-    fetchNowPlayingMovies: () => fetchMovies('/movies/theater/released')
+    fetchNowPlayingMovies: () => fetchMovies('/movies/theater/released'),
+    fetchStreamingMovieDetail: (id: string) => fetchStreamingMovieDetail(id),
+    fetchTheaterMovieDetail: (id: string) => fetchTheaterMovieDetail(id)
 } 
